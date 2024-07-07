2978684c9fae5906d4734bb3b8be6756929ff04d
Siddique Abdul <szuber558@gmail.com>
2024-07-07 18:23:46 +0530

    completed design

--- script.js
+++ script.js
@@ -37,11 +37,6 @@ function runAnimations() {
     "-=.4"
   );
 
-  swiper.on("slideChange", function () {
-    runAnimations();
-  });
-  runAnimations();
-
   document.querySelectorAll(".btn").forEach((button) => {
     const overlay = button.querySelector(".btn-overlay");
     let hoverAnimation;
@@ -109,3 +104,151 @@ openMenu.addEventListener("click", function () {
 closeMenu.addEventListener("click", function () {
   tl.reverse();
 });
+
+// form functions
+
+var tlLogin = gsap.timeline();
+var openLogin = document.querySelector("#loginBtn i");
+var closeLogin = document.querySelector("#closeLogin i");
+var closeUser = document.querySelector("#closeUser i");
+var createAccountLink = document.getElementById("createAccountLink");
+var backToLoginLink = document.getElementById("backToLoginLink");
+var cartDrawer = document.getElementById("cartBtn");
+var closeCartDrawer = document.querySelector("#closeCart i");
+var mobileCartOpen = document.getElementById("openMobileCart");
+var openMobileSidebar = document.getElementById("mobileMenu");
+var closeMobileSidebar = document.getElementById("navbarClose");
+var navDrawer = document.getElementById("navDrawer");
+var openMobileSearch = document.getElementById("openMobileSearch");
+
+var tlNav = gsap.timeline();
+
+tlNav.to(navDrawer, {
+  right: 0,
+  duration:0.5,
+});
+tlNav.from("#navDrawerList li", {
+  x: 30,
+  duration: 0.1,
+  opacity: 0,
+  stagger: 0.1,
+  ease: "power1.out",
+});
+tlNav.pause();
+openMobileSidebar.addEventListener("click", function () {
+  tlNav.play();
+});
+closeMobileSidebar.addEventListener("click", function () {
+  tlNav.reverse();
+});
+
+openMobileSearch.addEventListener("click", function () {
+  tl.play()
+});
+
+function cartOpenClose() {
+  cartDrawer.addEventListener("click", function () {
+    tlLogin.to("#cartDrawer", {
+      right: 0,
+      duration: 0.5,
+    });
+  });
+  closeCartDrawer.addEventListener("click", function () {
+    tlLogin.to("#cartDrawer", {
+      right: "-100%",
+      duration: 0.5,
+    });
+  });
+}
+cartOpenClose();
+
+function openCartDrawer() {
+  mobileCartOpen.addEventListener("click", function () {
+    tlLogin.to("#cartDrawer", {
+      right: 0,
+      duration: 0.5,
+    });
+  });
+}
+
+function login() {
+  openLogin.addEventListener("click", function () {
+    tlLogin.fromTo(
+      "#overlayLogin",
+      {
+        display: "none",
+        opacity: 0,
+        scale: 0.5,
+      },
+      {
+        display: "flex",
+        duration: 0.2,
+        opacity: 1,
+        scale: 1,
+      }
+    );
+  });
+}
+login();
+
+backToLoginLink.addEventListener("click", function () {
+  tlLogin.to("#overlayCreateAccount", {
+    display: "none",
+  });
+  tlLogin.fromTo(
+    "#overlayLogin",
+    {
+      display: "none",
+      opacity: 0,
+      scale: 0.5,
+    },
+    {
+      display: "flex",
+      duration: 0.2,
+      opacity: 1,
+      scale: 1,
+    }
+  );
+});
+
+createAccountLink.addEventListener("click", function () {
+  tlLogin.to("#overlayLogin", {
+    display: "none",
+  });
+  tlLogin.fromTo(
+    "#overlayCreateAccount",
+    {
+      display: "none",
+      opacity: 0,
+      scale: 0.5,
+    },
+    {
+      display: "flex",
+      duration: 0.2,
+      opacity: 1,
+      scale: 1,
+    }
+  );
+});
+
+closeLogin.addEventListener("click", function () {
+  tlLogin.to("#overlayLogin", {
+    display: "none",
+    y: -50,
+    duration: 0.4,
+    opacity: 0,
+  });
+});
+closeUser.addEventListener("click", function () {
+  tlLogin.to("#overlayCreateAccount", {
+    display: "none",
+    y: -50,
+    duration: 0.4,
+    opacity: 0,
+  });
+});
+
+swiper.on("slideChange", function () {
+  runAnimations();
+});
+runAnimations();
